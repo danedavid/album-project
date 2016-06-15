fa028e35680d46f7c38ce4cd7cc53c0602b29b27
danegit <dndavid102@gmail.com>
2016-06-15 09:35:59 +0530

    Start and stop functions added

--- js/cube.js
+++ js/cube.js
@@ -3,7 +3,7 @@ Type: JavaScript
 Description: To view and rotate the cube
 */
 
-
+var flag=0;
 var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
 
@@ -12,15 +12,34 @@ renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
 var geometry = new THREE.BoxGeometry( 2, 2, 2, 5, 5, 1);
-var material = new THREE.MeshBasicMaterial( {color: 0xEE0000, wireframe: true, wireframeLinewidth: 3} );
+var material = new THREE.MeshBasicMaterial( {color: 0xEE00EE, wireframe: true, wireframeLinewidth: 1} );
 var cube = new THREE.Mesh( geometry, material );
 scene.add( cube );
 
 camera.position.z = 4;
 
+renderer.render(scene,camera);
+
 function animate() {
 	requestAnimationFrame(animate);
-	cube.rotation.x += 0.01;
-	renderer.render(scene,camera);
+
+	if(flag==1){
+		cube.rotation.x += 0.01;
+		cube.rotation.y += 0.02;
+		renderer.render(scene,camera);
+	}
+	if(flag==2){
+		renderer.render(scene,camera);
+	}
+}
+
+function begin() {
+	if(flag==0)
+		animate();
+	flag=1;
+}
+
+function stop() {
+	flag=2;
 }
 
